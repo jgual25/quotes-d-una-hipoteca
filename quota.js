5231dba69f1b6e225392d210a73b487b25c86256
Your Name <you@example.com>
2018-01-22 14:46:21 +0100

    cquota mensual

--- quota.js
+++ quota.js
@@ -1,17 +1,36 @@
 function calcular(){
     //aqui crida a totes ses funcions
+   var quota = Math.round(capital()*interes()/(100*(1-Math.pow(1+interes()/100,-plazos())))*100)/100;
+   document.getElementById("response").innerHTML= "La quantitat mensual a abonar surt a: "+quota;
+   return false;
 }
+
 
 function diavalid(){
-    var day = document.getElementById("dia").value;
+    var day = new Date(document.getElementById("dia").value);
+    alert(day.getDate());
+    day = day.getDate();
+    if(day == 1){
+        return day; 
+    }else{
+        return null;
+        alert ("El dia inicial ha de començar a dia primer de mes.");
+       }
 }
 
-function capital(){
+function capital(num){
     var num= document.getElementById("capital").value;
+    var num = parseInt(num);
+    if(num <1000 || num>1000000){
+        alert ("El capital ha de ser com a mínim 1.000 i com a màxim 1.000.000.")
+    }else{
+        return num;
+    }
+    
 }
 function interes(){
     var tipo= document.getElementById("interes").value;
-    var resultat=((tipo*10)/100)/12;
+    var resultat=((tipo*10/100)/12);
     return resultat;
 }
 function plazos(){
