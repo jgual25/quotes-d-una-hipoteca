755afd8e0f7dd2e1f01734dd3cd23f3d3cf37b0e
Your Name <you@example.com>
2018-01-25 13:02:50 +0100

    diavalid +1

--- quota.js
+++ quota.js
@@ -1,43 +1,29 @@
 function calcular(){
     //aqui crida a totes ses funcions
    var quota = Math.round(capital()*interes()/(100*(1-Math.pow(1+interes()/100,-plazos())))*100)/100;
-   document.getElementById("response").innerHTML= "La quantitat mensual a abonar surt a: "+quota+" i l'hipoteca s'acabarà de pagar dia";
+   document.getElementById("response").innerHTML= "La quantitat mensual a abonar surt a: "+quota+" i l'hipoteca s'acabarà de pagar dia "+diavalid();
    return false;
 }
 
 
 function diavalid(){
     var day = new Date(document.getElementById("dia").value);
-    alert(day.getDate());
-    day = day.getDate();
-    if(day == 1){
-        return day; 
+    if (day.getDate() != 1){
+        alert ("La data inicial a pagar ha de ser el dia primer de mes");
+        window.location.href = "index.html";
+
     }else{
-        return null;
-        alert ("El dia inicial ha de començar a dia primer de mes.");
-       }
+    day.setMonth((day.getMonth()+1)+plazos());
+    return day.getDate()+"/"+day.getMonth()+"/"+day.getFullYear();
+    }
 }
 
-/*function calcularDiasAusencia(fechaIni, fechaFin) {
-    var diaEnMils = 1000 * 60 * 60 * 24,
-        desde = new Date(fechaIni.substr(0, 10)),
-        hasta = new Date(fechaFin.substr(0, 10)),
-        diff = hasta.getTime() - desde.getTime() + diaEnMils;// +1 incluir el dia de ini
-    return diff / diaEnMils;
-  }
-  
-  document.getElementById('calcular').addEventListener('click', function() {
-    document.getElementById('dias').innerText = calcularDiasAusencia(
-      document.getElementById('fechaIni').value,
-      document.getElementById('fechaFin').value
-    );
-});*/
 
 function capital(num){
     var num= document.getElementById("capital").value;
     var num = parseInt(num);
     if(num <1000 || num>1000000){
-        alert ("El capital ha de ser com a mínim 1.000 i com a màxim 1.000.000.")
+        alert ("El capital ha de ser com a mínim 1.000 i com a màxim 1.000.000.");
     }else{
         return num;
     }
